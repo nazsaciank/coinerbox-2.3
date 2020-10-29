428d97f1b44a4592121b36df070473ffff251191
Nazareno Ciancaglini <nazareno.ciancaglini@gmail.com>
2020-10-29 20:10:46 -0300

    Feature: fetch kline from finex if enabled

--- src/containers/HistoryElement/index.tsx
+++ src/containers/HistoryElement/index.tsx
@@ -13,7 +13,6 @@ import {
     setDepositStatusColor,
     setTradesType,
     setWithdrawStatusColor,
-    uppercase,
 } from '../../helpers';
 import {
     currenciesFetch,
@@ -189,7 +188,7 @@ class HistoryComponent extends React.Component<Props> {
                 return [
                     <div className="pg-history-elem__hide" key={txid || rid}><a href={blockchainLink} target="_blank" rel="noopener noreferrer">{txid || rid}</a></div>,
                     localeDate(created_at, 'fullDate'),
-                    uppercase(currency),
+                    currency && currency.toUpperCase(),
                     wallet && preciseData(amount, wallet.fixed),
                     fee,
                     <span style={{ color: setWithdrawStatusColor(item.state) }} key={txid || rid}>{state}</span>,

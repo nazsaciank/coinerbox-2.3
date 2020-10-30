799913b134f9161c694a22cf6bbe5e201f5de47d
Nazareno Ciancaglini <nazareno.ciancaglini@gmail.com>
2020-10-30 11:07:04 -0300

    Fix: Incremental update & remove unnecessary sorts

--- src/containers/OrderBook/index.tsx
+++ src/containers/OrderBook/index.tsx
@@ -4,7 +4,7 @@ import classNames from 'classnames';
 import * as React from 'react';
 import { InjectedIntlProps, injectIntl } from 'react-intl';
 import { connect, MapDispatchToPropsFunction } from 'react-redux';
-import { accumulateVolume, calcMaxVolume, sortAsks, sortBids } from '../../helpers';
+import { accumulateVolume, calcMaxVolume } from '../../helpers';
 import { colors } from '../../constants';
 import {
     Market,
@@ -100,7 +100,7 @@ class OrderBookContainer extends React.Component<Props, State> {
                 <div className={'cr-table-header__content'}>
                     {this.props.intl.formatMessage({id: 'page.body.trade.orderbook'})}
                 </div>
-                {orderBookLoading ? <div className="pg-combined-order-book-loader"><Spinner animation="border" variant="primary" /></div> : this.orderBook(sortBids(bids), sortAsks(asks))}
+                {orderBookLoading ? <div className="pg-combined-order-book-loader"><Spinner animation="border" variant="primary" /></div> : this.orderBook(bids, asks)}
             </div>
         );
     }
